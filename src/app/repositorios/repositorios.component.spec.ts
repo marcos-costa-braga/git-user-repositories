931f9ce24b7730d1eb72138b5882ccb9d3f2e408
maxnoturno <marcos_costa_braga@hotmail.com>
2021-03-24 02:27:27 -0300

    test: import http and router modules

--- src/app/repositorios/repositorios.component.spec.ts
+++ src/app/repositorios/repositorios.component.spec.ts
@@ -1,16 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { RepositoriosComponent } from './repositorios.component';
 
 describe('RepositoriosComponent', () => {
   let component: RepositoriosComponent;
   let fixture: ComponentFixture<RepositoriosComponent>;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientTestingModule ],
       declarations: [ RepositoriosComponent ]
     })
     .compileComponents();
+    
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
   }));
 
   beforeEach(() => {
--- src/app/usuarios/usuarios.component.spec.ts
+++ src/app/usuarios/usuarios.component.spec.ts
@@ -1,16 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { UsuariosComponent } from './usuarios.component';
 
 describe('UsuariosComponent', () => {
   let component: UsuariosComponent;
   let fixture: ComponentFixture<UsuariosComponent>;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
       declarations: [ UsuariosComponent ]
     })
     .compileComponents();
+    
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
   }));
 
   beforeEach(() => {
