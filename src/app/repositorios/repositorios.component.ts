b1a2abf493bcdef4496d480ca0e127bc403bafc3
maxnoturno <marcos_costa_braga@hotmail.com>
2021-03-24 00:05:05 -0300

    feat: repositorios e usuarios components

---
+++ src/app/repositorios/repositorios.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-repositorios',
+  templateUrl: './repositorios.component.html',
+  styleUrls: ['./repositorios.component.css']
+})
+export class RepositoriosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
