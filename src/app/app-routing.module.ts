6e5d39ed5851883f4df20ee392bb3c07c3f0e28d
maxnoturno <marcos_costa_braga@hotmail.com>
2021-03-24 00:26:48 -0300

    feat: app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { RepositoriosComponent } from './repositorios/repositorios.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/usuarios",
+    pathMatch: "full"
+  }, {
+    path: "usuarios",
+    component: UsuariosComponent
+  }, {
+    path: 'usuarios/:usuarioName/repositorios',
+    component: RepositoriosComponent
+  }, {
+    path: '**',
+    redirectTo: "/usuarios"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
