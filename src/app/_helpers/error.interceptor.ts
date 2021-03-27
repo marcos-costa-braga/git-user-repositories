daf71998e447df4fa90e50e2222a4bac581e2983
maxnoturno <marcos_costa_braga@hotmail.com>
2021-03-26 22:40:40 -0300

    feat: snackbar alert

--- src/app/_helpers/error.interceptor.ts
+++ src/app/_helpers/error.interceptor.ts
@@ -2,19 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor( ) {}
+    constructor(
+        private snackBar: MatSnackBar
+    ) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request).pipe(catchError(err => {
             if ([403].includes(err.status)) {
                 console.log(err)
-                if(err.error.documentation_url == "https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting")
-                    //TODO colocar swal
-                    console.log('era pra ter um swal aqui no erro')
-                    return;
+                if (err.error.documentation_url == "https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting")
+                this.snackBar.open('Limite de requisições atingido', null, {
+                    duration: 2000,
+                    panelClass: ['text-white', 'font-weight-bold']
+                  });
+                return;
             }
             const error = err.error?.message || err.statusText;
             return throwError(error);
